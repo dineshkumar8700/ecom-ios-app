58bc8df6f4fb0958b993562aefa5e8681ada0e81
Dinesh Kumar <mk9211620@gmail.com>
2026-06-24 16:35:29 +0530

    Abstracts ProductListView into small sub views

--- ecom-mvvm/Presentation/Views/Home.swift
+++ ecom-mvvm/Presentation/Views/Home.swift
@@ -8,6 +8,64 @@
 import Foundation
 import SwiftUI
 
+struct ProductImage: View {
+    let image: String
+    
+    var body: some View {
+        AsyncImage(url: URL(string: image)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+        } placeholder: {
+            Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(.gray)
+        }.padding(10)
+    }
+}
+
+struct ProductTitle: View {
+    let title: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.system(size: 14))
+                .lineSpacing(0.4)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+struct ProductPrice: View {
+    let price: Double
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("$\(price.formatted())")
+                .font(.system(size: 14))
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+struct ProductDetailRow: View {
+    let product: Product
+    
+    var body: some View {
+        HStack {
+            ProductImage(image: product.image)
+            ProductTitle(title: product.title)
+            Spacer()
+            ProductPrice(price: product.price)
+    
+        }
+    }
+}
+
 struct ProductListView: View {
     @StateObject private var vm: ProductListViewModel
     
@@ -22,34 +80,10 @@ struct ProductListView: View {
                 .fontWeight(.bold)
             
             List(vm.products) { product in
-                HStack {
-                    AsyncImage(url: URL(string: product.image)) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80)
-                    } placeholder: {
-                        Image(systemName: "photo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 80, height: 80)
-                                .foregroundStyle(.gray)
-                    }.padding(10)
-                        
-                    Text(product.title)
-                        .font(.system(size: 14))
-                        .lineSpacing(0.4)
-                        .fontWeight(.semibold)
-                    Spacer()
-                    
-                    Text("$\(product.price.formatted())")
-                        .font(.system(size: 14))
-                        .fontWeight(.semibold)
-                }
+                ProductDetailRow(product: product)
 
             }.listStyle(.plain)
                 
-            Spacer()
         }
         .task {
             await vm.fetchProducts()
